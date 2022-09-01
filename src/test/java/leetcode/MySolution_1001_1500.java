d7870de24e8d7843bb23ade42b34348d106a8572
kirito <504831261@qq.com>
2022-09-01 14:19:45 +0800

    no1475

--- src/test/java/leetcode/MySolution_1001_1500.java
+++ src/test/java/leetcode/MySolution_1001_1500.java
@@ -409,7 +409,17 @@ public class MySolution_1001_1500 {
     }
 
     public int[] finalPrices(int[] prices) {
-        return null;
+        int n = prices.length;
+        int[] res = new int[n];
+        Deque<Integer> deque = new ArrayDeque<>();
+        for (int i = n - 1; i >= 0; --i) {
+            while (!deque.isEmpty() && prices[i] < deque.peek()) {
+                deque.pop();
+            }
+            res[i] = deque.isEmpty() ? prices[i] : prices[i] - deque.peek();
+            deque.push(prices[i]);
+        }
+        return res;
     }
 
 }
