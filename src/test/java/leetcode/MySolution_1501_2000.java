d8fdf4aea269fa76c5c273a13b9828359c8b2658
kirito <504831261@qq.com>
2022-09-04 17:42:42 +0800

    no1582

--- src/test/java/leetcode/MySolution_1501_2000.java
+++ src/test/java/leetcode/MySolution_1501_2000.java
@@ -8,7 +8,29 @@ package leetcode;
 public class MySolution_1501_2000 {
 
     public int numSpecial(int[][] mat) {
-        return 0;
+        //行数
+        int row = mat.length;
+        //列数
+        int col = mat[0].length;
+        //每行的和
+        int[] rowSum = new int[row];
+        //每列的和
+        int[] colSUm = new int[col];
+        int sum = 0;
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                rowSum[i] = rowSum[i] + mat[i][j];
+                colSUm[j] = colSUm[j] + mat[i][j];
+            }
+        }
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                if (mat[i][j] == 1 && rowSum[i] == 1 && colSUm[j] == 1) {
+                    ++sum;
+                }
+            }
+        }
+        return sum;
     }
 
 }
