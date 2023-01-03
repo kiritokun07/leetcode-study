e1258a2cf731b3e88de27d75212095a3c0ab16b1
kirito <504831261@qq.com>
2023-01-03 11:38:45 +0800

    no2042

--- src/test/java/leetcode/MyClientTest_2001_2500.java
+++ src/test/java/leetcode/MyClientTest_2001_2500.java
@@ -19,4 +19,17 @@ public class MyClientTest_2001_2500 {
         Assertions.assertEquals(solution.minimumMoves("OOOO"), 0);
     }
 
+    /**
+     * 2042. 检查句子中的数字是否递增
+     * 简单
+     * https://leetcode.cn/problems/check-if-numbers-are-ascending-in-a-sentence/
+     */
+    @Test
+    public void no2042() {
+        Assertions.assertTrue(solution.areNumbersAscending("1 box has 3 blue 4 red 6 green and 12 yellow marbles"));
+        Assertions.assertFalse(solution.areNumbersAscending("hello world 5 x 5"));
+        Assertions.assertFalse(solution.areNumbersAscending("sunset is at 7 51 pm overnight lows will be in the low 50 and 60 s"));
+        Assertions.assertTrue(solution.areNumbersAscending("4 5 11 26"));
+    }
+
 }
--- src/test/java/leetcode/MySolution_2001_2500.java
+++ src/test/java/leetcode/MySolution_2001_2500.java
@@ -16,4 +16,32 @@ public class MySolution_2001_2500 {
         return res;
     }
 
+    public static void main(String[] args) {
+        String s = "123";
+        char c = s.charAt(0);
+        if (c == 49) {
+            System.out.println("yes");
+        }
+    }
+
+    public boolean areNumbersAscending(String s) {
+        String[] s1 = s.split(" ");
+        int lastNum = -1;
+        for (String s2 : s1) {
+            //如果s2包含数字就进行解析，否则就跳过
+            char c = s2.charAt(0);
+            if (c > 57) {
+                continue;
+            }
+            //如果数字大于上次的数字就ok，否则就返回false
+            int s2Int = Integer.parseInt(s2);
+            if (s2Int <= lastNum) {
+                return false;
+            }
+            //记录最新的数字
+            lastNum = s2Int;
+        }
+        return true;
+    }
+
 }
